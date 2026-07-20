0c08166f52dfe7b6ab59836805cee96630a55eae
Filip Václavík <filip@vaclavik.xyz>
2026-07-20 14:19:26 +0200

    feat: publish Diktato 3.0.0 cask

---
+++ Casks/diktato.rb
@@ -0,0 +1,23 @@
+cask "diktato" do
+  version "3.0.0"
+  sha256 "daddab498b393e4a71a8b18a0c69816867e578a7e4f02ad20bbd66c0259da711"
+
+  url "https://github.com/vaclavik-xyz/diktato-releases/releases/download/v#{version}/Diktato.dmg",
+      verified: "github.com/vaclavik-xyz/diktato-releases/"
+  name "Diktāto"
+  desc "Czech-first voice dictation with optional AI cleanup"
+  homepage "https://diktato.com/"
+
+  depends_on macos: :sonoma
+
+  app "Diktāto.app"
+
+  uninstall quit: "app.diktato.mac"
+
+  zap trash: [
+    "~/Library/Application Support/Diktato",
+    "~/Library/Caches/app.diktato.mac",
+    "~/Library/HTTPStorages/app.diktato.mac",
+    "~/Library/Preferences/app.diktato.mac.plist",
+  ]
+end
