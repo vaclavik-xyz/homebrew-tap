68f79bfee6dbf92aec3a2fcf99fe2f910a7812fe
Filip Václavík <filip@vaclavik.xyz>
2026-09-06 03:33:06 +0200

    chore: bump Diktato cask to 3.3.3

--- Casks/diktato.rb
+++ Casks/diktato.rb
@@ -1,6 +1,6 @@
 cask "diktato" do
-  version "3.3.2"
-  sha256 "e91b72574c86d52d58973e8fb24f573d089c19ecacec18a63356be3437b076e3"
+  version "3.3.3"
+  sha256 "e0950b8ccee481f0030bd577362fd330fe8e901dfc113ccaaaa65d272cbde47e"
 
   url "https://github.com/vaclavik-xyz/diktato-releases/releases/download/v#{version}/Diktato.dmg",
       verified: "github.com/vaclavik-xyz/diktato-releases/"
